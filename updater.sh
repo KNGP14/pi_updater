9e9f9b36c19c221c94ead0958e08ffc99848fb46
Kevin Neumann <KNGP14@users.noreply.github.com>
2024-07-27 10:13:58 +0200

    init

---
+++ updater.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo ""
+echo "======================"
+echo "= Repository-Updater ="
+echo "======================"
+echo ""
+
+updateRepository () {
+    echo "Updating '$1' ..."
+    cd "$1"
+    git reset --hard
+    git pull
+    echo ""
+}
+
+updateRepository "~/pi_watering"
+updateRepository "~/pi_lastmgmt"
